824525a534dfbd0a2653a0017a380c3ff5c23cfc
codemobiles <oldster189@gmail.com>
2018-12-19 11:28:53 +0700

    aaaaa

--- android/src/main/java/com/reactlibrary/RNDemoLibModule.java
+++ android/src/main/java/com/reactlibrary/RNDemoLibModule.java
@@ -7,6 +7,8 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.uimanager.IllegalViewOperationException;
+import com.facebook.react.uimanager.PixelUtil;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +16,7 @@ import java.util.Map;
 public class RNDemoLibModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
+    private final int[] mMeasureBuffer = new int[4];
 
     public RNDemoLibModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -38,5 +41,24 @@ public class RNDemoLibModule extends ReactContextBaseJavaModule {
     public void show(String message, int duration) {
         Toast.makeText(getReactApplicationContext(), message, duration).show();
     }
+
+    @ReactMethod
+    public void measureLayout(
+            int tag,
+            int ancestorTag,
+            Callback errorCallback,
+            Callback successCallback) {
+        try {
+            measureLayout(tag, ancestorTag, mMeasureBuffer);
+            float relativeX = PixelUtil.toDIPFromPixel(mMeasureBuffer[0]);
+            float relativeY = PixelUtil.toDIPFromPixel(mMeasureBuffer[1]);
+            float width = PixelUtil.toDIPFromPixel(mMeasureBuffer[2]);
+            float height = PixelUtil.toDIPFromPixel(mMeasureBuffer[3]);
+            successCallback.invoke(relativeX, relativeY, width, height);
+        } catch (IllegalViewOperationException e) {
+            errorCallback.invoke(e.getMessage());
+        }
+    }
+
 
 }
