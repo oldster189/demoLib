9469ffada699882c50b03979abd20055067931a6
codemobiles <oldster189@gmail.com>
2018-12-19 11:13:35 +0700

    aaaaa

--- android/src/main/java/com/reactlibrary/RNDemoLibModule.java
+++ android/src/main/java/com/reactlibrary/RNDemoLibModule.java
@@ -1,9 +1,15 @@
 package com.reactlibrary;
 
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RNDemoLibModule extends ReactContextBaseJavaModule {
 
@@ -20,8 +26,7 @@ public class RNDemoLibModule extends ReactContextBaseJavaModule {
     }
  
    @Override
-  public @Nullable
-  Map<String, Object> getConstants() {
+    public @Nullable Map<String, Object> getConstants() {
     HashMap<String, Object> constants = new HashMap<String, Object>();
             constants.put("SHORT", Toast.LENGTH_SHORT);
             constants.put("LONG", Toast.LENGTH_LONG);
